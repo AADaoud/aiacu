aec93ff44a5b77f6818b50dee18174a830ad121f
AADaoud <97793913+AADaoud@users.noreply.github.com>
2023-05-25 00:31:55 +0300

    index error fixed for unix

--- ingest.py
+++ ingest.py
@@ -1,4 +1,3 @@
-"""This is the logic for ingesting Notion data into LangChain."""
 from pathlib import Path
 from langchain.text_splitter import CharacterTextSplitter
 import faiss
@@ -11,8 +10,6 @@ import re
 # Use Streamlit's secret management for secure handling of API keys
 api_key = st.secrets["OPENAI_API_KEY"]
 
-
-# Here we load in the data in the format that Notion exports it in.
 ps = list(Path(".Lectures/").glob("**/*.md"))
 
 data = []
@@ -22,16 +19,13 @@ for p in ps:
     with open(p, encoding="utf-8", errors='replace') as f:
         content = f.read()
         data.append(content)
-        # Extract Key Topics from the header if present
         topic_match = re.search(r"# Key Topics\n((?:.+\n)*)", content)
         if topic_match:
             topics.append(topic_match.group(1).strip())
         else:
-            topics.append(None)  # Omit key topics for files that don't contain them
-    sources.append(p)
+            topics.append(None) 
+    sources.append(str(p)) # convert pathlib.Path to string here
 
-# Here we split the documents, as needed, into smaller chunks.
-# We do this due to the context limits of the LLMs.
 text_splitter = CharacterTextSplitter(chunk_size=1500, separator="\n")
 docs = []
 metadatas = []
@@ -43,8 +37,6 @@ for i, d in enumerate(data):
     else:
         metadatas.extend([{"source": sources[i]}] * len(splits))
 
-
-# Here we create a vector store from the documents and save it to disk.
 store = FAISS.from_texts(docs, OpenAIEmbeddings(client=any, openai_api_key=api_key), metadatas=metadatas)
 faiss.write_index(store.index, "docs.index")
 store.index = None
